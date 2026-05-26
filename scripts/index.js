7a12319f300acce26ea2ef4f0268162f93adeb70
Synthird <105394020+Synthird@users.noreply.github.com>
2026-05-26 16:01:16 -0700

    feat: remove the text showing how the button was pressed after a few seconds of being displayed

--- scripts/index.js
+++ scripts/index.js
@@ -4,5 +4,6 @@ const button = document.querySelector("button"),
 button.addEventListener("pointerdown", event => {
 	const pointerType = event.pointerType;
 
-	displayPressedMethod.textContent = `The button was pressed by ${pointerType}`;
+	displayPressedMethod.textContent = `The button was pressed by ${pointerType}.`;
+	setTimeout(() => displayPressedMethod.textContent = "", 2000);
 });
