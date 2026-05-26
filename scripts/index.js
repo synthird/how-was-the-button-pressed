51b67b02537cf7749bcddb7d3011c7e5e5f6d196
Synthird <105394020+Synthird@users.noreply.github.com>
2026-05-26 15:54:59 -0700

    feat: show how the button was pressed

--- scripts/index.js
+++ scripts/index.js
@@ -3,5 +3,6 @@ const button = document.querySelector("button"),
 
 button.addEventListener("pointerdown", event => {
 	const pointerType = event.pointerType;
-	console.log(pointerType);
+
+	displayPressedMethod.textContent = `The button was pressed by ${pointerType}`;
 });
