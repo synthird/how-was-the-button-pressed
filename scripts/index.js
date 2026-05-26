891ee391cc915a7690c2ab902c9dcd2a23f01f89
Synthird <105394020+Synthird@users.noreply.github.com>
2026-05-26 16:05:28 -0700

    feat: use an exclamation mark instead of a period in displaying how the button was pressed

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,6 @@ const button = document.querySelector("button"),
 	displayPressedMethod = document.getElementById("display-pressed-method");
 
 button.addEventListener("pointerdown", event => {
-	displayPressedMethod.textContent = `The button was pressed by ${event.pointerType}.`;
+	displayPressedMethod.textContent = `The button was pressed by ${event.pointerType}!`;
 	setTimeout(() => displayPressedMethod.textContent = "", 2000);
 });
